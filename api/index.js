67ecb724a65b28f98cb8d8584a1489b58aa5e09d
PunitChawla <chawlapunit79@gmail.com>
2025-05-22 10:59:45 +0530

    add api folder

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Note = require("./models/struct");
+const bodyparser = require("body-parser");
+const app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+const port = process.env.PORT || 4000;
+const DBSTRING =
+    "mongodb+srv://raushan8292kumardeo:ra0409ku@cluster0.evi9cj4.mongodb.net/notesDB";
+mongoose.connect(DBSTRING).then(() => {
+    app.get("/", (req, res) => {
+        let response = { message: "API working fine!!!" };
+        res.json(response);
+    });
+    const rootrouter = require("./routes/route");
+    app.use("/notes", rootrouter);
+});
+
+app.get("/resp", (req, res) => {
+    res.send("Hellllo BOOSS");
+});
+
